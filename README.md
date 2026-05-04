30c45d802a3da8e3a6ec7f99ad03b51cf8d94546
hifzhil <hifzhil12@gmail.com>
2026-05-04 23:54:36 +0800

    spawn formula models success, and it moving now in gazebo using gz-sim-ackermann-steering-system

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # robot-simulator
 for my learning stuff about ROS2
+
+Thanks to:
+https://github.com/2024828/car_nav2
---
+++ spawn_robot/launch/world.launch.py
@@ -0,0 +1,47 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+
+    # Declare the world file argument
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='default.sdf',
+        description='Name of the Gazebo world file to load'
+    )
+
+    # Get package directories
+    pkg_spawn_robot = get_package_share_directory('spawn_robot')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    # Update GZ_SIM_RESOURCE_PATH with the path to worlds folder
+    gazebo_models_path = os.path.join(pkg_spawn_robot, 'worlds')
+
+    # Include Gazebo launch file
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'),
+        ),
+        launch_arguments={
+            'gz_args': [PathJoinSubstitution([
+                pkg_spawn_robot,
+                'worlds',
+                LaunchConfiguration('world')
+            ]),
+            TextSubstitution(text=' -r -v -v1')],
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    # Create launch description
+    launchDescriptionObject = LaunchDescription()
+
+    # Add actions
+    launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(gazebo_launch)
+
+    return launchDescriptionObject
